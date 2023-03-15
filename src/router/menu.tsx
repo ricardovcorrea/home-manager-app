70c7008c70aed2a380eb4a616272e81342da1218
Ricardo Vaz <ricardo.vaz@mindera.com>
2023-03-15 20:11:03 +0000

    first commit

---
+++ src/router/menu.tsx
@@ -0,0 +1,16 @@
+export const AppMenu = () => {
+  return (
+    <div className='menu'>
+      <nav>
+        <ul>
+          <li>
+            <a href={`/`}>Home</a>
+          </li>
+          <li>
+            <a href={`/reader/`}>Reader</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
