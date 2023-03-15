778c35081cac4fb1d3bc60d7dd2eb5bd59119b0e
Ricardo Vaz <ricardo.vaz@mindera.com>
2023-03-15 21:22:39 +0000

    added available cameras

--- src/components/barCodeReader.tsx
+++ src/components/barCodeReader.tsx
@@ -1,45 +1,50 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Html5Qrcode } from 'html5-qrcode';
 
-export const BarCodeReader = () => {
+interface ICameraDevice {
+  id: string;
+  label: string;
+}
+interface IBarCodeReaderProps {
+  onRead: (code: string) => void;
+}
+
+export const BarCodeReader = (props: IBarCodeReaderProps) => {
   const readerRef = useRef<Html5Qrcode>();
-  const [availableCameras, setAvailableCameras] = useState<any[]>([]);
+  const [availableCameras, setAvailableCameras] = useState<ICameraDevice[]>([]);
+  const [selectedCamera, setSelectedCamera] = useState<ICameraDevice>();
 
   useEffect(() => {
-    if (readerRef.current === undefined) {
-      Html5Qrcode.getCameras().then((cameras) => {
-        if (cameras === undefined || cameras.length === 0) {
-          return;
-        }
-
-        setAvailableCameras(cameras);
-      });
+    if (readerRef.current !== undefined) {
+      return;
     }
+
+    Html5Qrcode.getCameras().then((cameras) => {
+      if (cameras === undefined || cameras.length === 0) {
+        return;
+      }
+
+      setAvailableCameras(cameras);
+      setSelectedCamera(cameras[0]);
+    });
   }, []);
 
   useEffect(() => {
-    if (
-      availableCameras === undefined ||
-      availableCameras.length === 0 ||
-      readerRef.current !== undefined
-    ) {
+    if (selectedCamera === undefined || readerRef.current !== undefined) {
       return;
     }
 
     readerRef.current = new Html5Qrcode('barcode-reader');
 
     readerRef.current.start(
-      availableCameras[0].id,
+      selectedCamera.id,
       {
         fps: 10,
       },
       (decodedText, decodedResult) => {
-        console.log(decodedText);
-        console.log(decodedResult);
+        props.onRead(decodedText);
       },
-      (errorMessage) => {
-        // ignore errors
-      }
+      () => {}
     );
 
     return () => {
@@ -58,7 +63,33 @@ export const BarCodeReader = () => {
         readerRef.current.clear();
       });
     };
-  }, [availableCameras]);
+  }, [selectedCamera]);
 
-  return <div className={'barcode-reader'} id={'barcode-reader'}></div>;
+  return (
+    <div className={'barcode-reader'}>
+      <div id={'barcode-reader'} />
+      <div className={'barcode-reader__line'} />
+      <div className={'barcode-reader__camera-name'}>
+        {selectedCamera?.label}
+      </div>
+      <select
+        className={'barcode-reader__available-cameras'}
+        onChange={(e) => {
+          const aCam = availableCameras.find((ac) => ac.id === e.target.value);
+
+          if (aCam === null) {
+            return;
+          }
+
+          setSelectedCamera(aCam);
+        }}
+      >
+        {availableCameras.map((camera) => (
+          <option value={camera.id} key={camera.id}>
+            {camera.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
--- src/pages/reader.tsx
+++ src/pages/reader.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BarCodeReader } from '../components';
 
 export const ReaderPage = () => {
+  const [reads, setReads] = useState<string[]>([]);
+
   return (
     <div className={'reader-page'}>
       <div className={'reader-page__content'}>
         <h1>Start scanning!</h1>
         <div className='reader-page__reader'>
-          <BarCodeReader />
+          <BarCodeReader
+            onRead={(code) => {
+              setReads([...reads, code]);
+            }}
+          />
+        </div>
+        <div>
+          {reads.map((r, i) => (
+            <div key={i}>{r}</div>
+          ))}
         </div>
         <Link to={'/'}>Home</Link>
       </div>
